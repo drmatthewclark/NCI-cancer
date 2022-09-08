083822cf55a35ed0c3bdeb175c0fd37fa4e6b884
Matthew Clark <matthewclark@Matthews-MacBook-Pro.local>
2022-09-08 10:28:34 -0400

    improve error handling

--- read_invivo.py
+++ read_invivo.py
@@ -1,15 +1,25 @@
 from urllib.request import urlopen
+import pubchempy
+from pubchempy import get_compounds
+from rdkit import Chem
 import sys
 import os
 import time
 
-fname = 'invivodec02' # file name
+fname = 'invivodec02-sorted' # file name
+output_file = 'NCI-data.sdf'
+
 ckpoint = 'checkpoint.cpk'
+
+USE_PUBMED = False
 
 delim = '|'  # file delimiter
 
 # columns in file.  I don't know what the last one is
 columns = ['tumor', 'strain', 'site', 'schedule', 'route', 'vehicle', 'parameter', 'NSC', 'unit', 'dose', 'T/C', 'endpoint', 'survived', 'x' ]
+
+def errormsg(msg):
+	sys.stderr.write(msg + '\n')
 
 
 def read_dict(fname):
@@ -74,7 +84,7 @@ def duration(schedule):
 	duration = float(every * count)
 	# convert to days
 	if time == 'H' : duration /= 24.0
-	if time == 'M' : duration /= 24.0 * 60
+	if time == 'M' : duration /= (24.0 * 60)
 	return duration
 
 
@@ -94,47 +104,98 @@ def assess(datamap):
 
 def add_tags(sdfile, datamap):
 	sdfile = sdfile[:sdfile.find('$$$$')]
+	if sdfile[-1] != '\n' : sdfile += '\n'
+
 	for tag in datamap:
 		sdfile += '>  <' + tag + '>\n'
 		sdfile += str(datamap[tag]) + '\n\n'
 
-	sdfile += '$$$$'
+	sdfile += '$$$$\n'
 	return sdfile
 
 
-def createurl(number):
+def getsdf(identifier):
+	if USE_PUBMED:
+		return getsdf_pubmed(identifier)
+	else:
+		return getsdf_nci(identifier)
+
+
+def getsdf_nci(number):
 	queryformat = 'sdf'
-	queryurl = ''
-	done = False
-	while not done:
+	timeout = 10
+	sdfile = None
+	number = int(number)
+	queryurl = 'https://cactus.nci.nih.gov/chemical/structure/NSC%d/file?format=%s' % (number, queryformat)
+
+	for i in range(2):
 		try:
-			queryurl = 'https://cactus.nci.nih.gov/chemical/structure/NSC%d/file?format=%s' % (number, queryformat)
-			done = True
-		except:
+			sdfile = urlopen(queryurl,timeout=timeout).read().decode('utf-8')
+
+		except Exception as e:
+			errormsg('sleeping: ' + str(e))
 			time.sleep(5)
+
+	if sdfile == None:	
+		errormsg('error fetching ' + queryurl  + ' ' + str(e))
+		errormsg('trying pubmed')
+		sdfile = getsdf_pubmed(number)
+
+	return sdfile
+
+
+def getsdf_pubmed(identifier):
+	sdfil = None
+
+	# try a few times in case is busy
+	for i in range(5):
+		try:
+			compound = get_compounds('NSC' + str(identifier), 'name')
+			break
+		except:
+			errormsg('sleeping')
+			time.sleep(10)
+
+
+	if compound is not None and len(compound) > 0:
+		compound = compound[0]
+		try:
+			smiles = compound.isomeric_smiles 
+			mol = Chem.MolFromSmiles(smiles)
+			sdfil = Chem.MolToMolBlock(mol)
+		except Exception as e:
+			errormsg('error converting smiles to sdf: ' + smiles)
+			errormsg(str(e))
 			
-	return queryurl
 
+	return sdfil	
 
-def processrecord(i, datamap):
+def processrecord(datamap):
 
 	global cache_hits, cache_miss, cached_structure
+	structure = None
 	cmpd = int(datamap['NSC'])
 
-	if i > 0 and i % log_interval == 0:
-		sys.stderr.write('wrote  %7d sdfiles cache_hit %6d cache_miss %6d \n' % (i, cache_hits, cache_miss) )
 
 	if cmpd in cached_structure:
 		cache_hits += 1
 		structure = cached_structure[cmpd]
 	else:
 		cache_miss += 1
-		url = createurl(cmpd)
-		structure = urlopen(url).read().decode('utf-8')
+		structure = getsdf(cmpd)
+		if len(cached_structure) > max_cache:
+			cached_structure = dict()
+
 		cached_structure[cmpd] = structure
 
-	structure = add_tags(structure, datamap)
-	print(structure)
+	if structure is not None:
+		structure = add_tags(structure, datamap)
+		with open(output_file, 'a') as f:
+			f.write(structure) # has newline at end already
+		return 0
+	else:
+		errormsg('error finding ' + str(cmpd) )
+		return 1
 
 #------------------------------start processing --------------------------
 
@@ -142,18 +203,27 @@ def processrecord(i, datamap):
 cached_structure = dict()
 cache_hits = 0
 cache_miss = 0
-log_interval = 1000
+max_cache = 100
+log_interval = 100
 lastread = -10
 
 if os.path.exists(ckpoint):
 	with open(ckpoint, 'r') as f:
 		lastread = int(f.read())
-		sys.stderr.write('restarting at line ' +  str(lastread) + '\n')
+		errormsg('restarting at line ' +  str(lastread))
 
 with open(fname, 'r') as file:
+	start = time.time()
+	errorcount = 0
 	for count, line in enumerate(file.readlines()):
 		if count <= lastread:
 			continue
+
+		if count  > 0 and count  % log_interval == 0:
+			elapsed = time.time() - start
+			recs_sec = count/elapsed
+			errormsg('wrote  %7d sdfiles cache_hit %6d cache_miss %6d error %5d rate %5.2f' 
+				% (count, cache_hits, cache_miss, errorcount, recs_sec) )
 
 		data = line.split(delim)
 		datamap = dict() 
@@ -167,12 +237,11 @@ with open(fname, 'r') as file:
 			datamap['duration'] = duration( datamap['schedule'])
 
 		datamap = assess(datamap)
-		processrecord(count, datamap)
+		errorcount += processrecord(datamap)
 
 		# remember last entry read for restarting
 		with open(ckpoint, 'w') as f:
 			f.write(str(count))
 
-
-sys.stderr.write('END')
+errormsg('END')
 
